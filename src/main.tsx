9bf61d1d0c0f1eed3379a52c10cff10aaeb3b57c
Timothy N. <timothynn08@gmail.com>
2026-08-27 16:42:42 +0300

    style: load proof section styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './overrides.css';
+import './proof.css';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
