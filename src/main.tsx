1287386824548d36befe316de4e40baedcb4117a
Timothy N. <timothynn08@gmail.com>
2026-08-27 16:35:22 +0300

    style: load Aivanta visual overrides after base styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import './overrides.css';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
