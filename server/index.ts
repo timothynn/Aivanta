37b6c45b9fdd9a4c568b32a244b7e6e8d5377d1b
Timothy N. <timothynn08@gmail.com>
2026-08-28 08:58:16 +0300

    feat: wire analytics store into server

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
 import { Pool } from 'pg';
+import { InMemoryAnalyticsStore, PostgresAnalyticsStore } from './adapters/postgresAnalyticsStore.js';
 import { InMemoryLeadStore } from './adapters/inMemoryLeadStore.js';
 import { PostgresLeadStore } from './adapters/postgresLeadStore.js';
 import { ConsoleLeadNotifier, ResendLeadNotifier } from './adapters/notifiers.js';
@@ -7,16 +8,16 @@ import { createApp } from './app.js';
 import { readConfig } from './config.js';
 
 const config = readConfig();
-
-const leadStore = config.databaseUrl
-  ? new PostgresLeadStore(new Pool({ connectionString: config.databaseUrl }))
-  : new InMemoryLeadStore();
+const pool = config.databaseUrl ? new Pool({ connectionString: config.databaseUrl }) : null;
+const leadStore = pool ? new PostgresLeadStore(pool) : new InMemoryLeadStore();
+const analyticsStore = pool ? new PostgresAnalyticsStore(pool) : new InMemoryAnalyticsStore();
 
 const app = await createApp({
   config,
   chatAssistant: createConfiguredSupportChatAssistant(config),
   leadStore,
   leadNotifier: config.resendApiKey ? new ResendLeadNotifier(config) : new ConsoleLeadNotifier(),
+  analyticsStore,
 });
 
 await app.listen({ host: '0.0.0.0', port: config.port });
